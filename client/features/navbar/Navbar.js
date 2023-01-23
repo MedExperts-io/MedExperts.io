92b001427e5ec55174450f9a72a19f71751dac14
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-01-23 11:40:23 -0500

    Removed style.css on nav and forms

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
@@ -17,8 +18,8 @@ const SiteNavbar = () => {
   };
 
   return (
-    <Navbar style={{ backgroundColor: "#FF6262" }} expand="lg">
-      <Container>
+    <Navbar collapseOnSelect style={{ backgroundColor: "#FF6262" }} expand="lg">
+      <Container fluid>
         <Navbar.Brand href="/">
           <img
             src="/MedExpert.png"
@@ -27,23 +28,44 @@ const SiteNavbar = () => {
             alt="Med Expert Logo"
           />
         </Navbar.Brand>
-        <Nav>
-          {" "}
-          {isLoggedIn ? (
-            <div>
-              <Link to="/home">Home</Link>
-              <button type="button" onClick={logoutAndRedirectHome}>
-                Logout
-              </button>
-            </div>
-          ) : (
-            // temporarily moved some logged-in user features to this section so I can visualize what they look like & until we hook up login features completely
-            <div>
-              <Link to="/home">Home</Link>
-              <Link to="/profile">Profile</Link>
-            </div>
-          )}
-        </Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            {isLoggedIn ? (
+              <div>
+                <Link to="/home">Home</Link>
+                <button type="button" onClick={logoutAndRedirectHome}>
+                  Logout
+                </button>
+              </div>
+            ) : (
+              // temporarily moved some logged-in user features to this section so I can visualize what they look like & until we hook up login features completely
+              <>
+                <Button size="sm" variant="light" style={{ padding: "0" }}>
+                  <Nav.Link to="/home">Home</Nav.Link>
+                </Button>
+                <Button variant="light">
+                  <Nav.Link to="/dashboard">Dashboard</Nav.Link>
+                </Button>
+                <Button variant="light">
+                  <Nav.Link to="/favorites">Favorites</Nav.Link>
+                </Button>
+              </>
+            )}
+          </Nav>
+          <Nav>
+            <NavDropdown title="Profile" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="/placeholder">
+                Placeholder 1
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/placeholder2">
+                Placeholder 2
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/signout">Sign out</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
