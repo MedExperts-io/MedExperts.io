9bbf9b74e90a3642fb970065844cac108f81dce0
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-01-23 21:12:44 -0500

    modified login form and login/signup container

--- client/features/auth/LoginOrSignup.js
+++ client/features/auth/LoginOrSignup.js
@@ -1,36 +1,61 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import Login from "./Login";
-import SignUp from "./SignUp";
+import SignUp from "./Signup";
 
 const LoginOrSignup = () => {
   return (
-    <Container className="p-4">
-      <Card className="mx-auto" style={{ maxWidth: "800px" }}>
-        <Col>
-          <center>
-            <Tabs
-              defaultActiveKey="login"
-              id="justify-tab-login"
-              className="mb-3"
-              justify
-            >
-              <Tab eventKey="login" title="Login">
-                {<Login />}
-              </Tab>
-              <Tab eventKey="signup" title="Sign Up">
-                {<SignUp />}
-              </Tab>
-            </Tabs>
-          </center>
+    <Container>
+      <Row className="vh-100 d-flex justify-content-center align-items-center">
+        <Col md={8} lg={6} xs={12}>
+          <Card className="shadow">
+            <Card.Body>
+              <Tabs
+                defaultActiveKey="login"
+                id="justify-tab-login"
+                className="mb-3"
+                justify
+              >
+                <Tab eventKey="login" title="Login">
+                  {<Login />}
+                </Tab>
+                <Tab eventKey="signup" title="Sign Up">
+                  {<SignUp />}
+                </Tab>
+              </Tabs>
+            </Card.Body>
+          </Card>
         </Col>
-      </Card>
-      <div className="p-2"></div>
+      </Row>
     </Container>
+
+    // <Container className="d-flex">
+    //   <
+    //     style={{
+    //       maxWidth: "500px",
+    //     }}
+    //   >
+    //     <Col>
+    //       {/* <center> */}
+    //       <Tabs
+    //         defaultActiveKey="login"
+    //         id="justify-tab-login"
+    //         className="mb-3"
+    //         justify
+    //       >
+    //         <Tab eventKey="login" title="Login">
+    //           {<Login />}
+    //         </Tab>
+    //         <Tab eventKey="signup" title="Sign Up">
+    //           {<SignUp />}
+    //         </Tab>
+    //       </Tabs>
+    //       {/* </center> */}
+    //     </Col>
+    //   </Card>
+    // </Container>
   );
 };
 export default LoginOrSignup;
