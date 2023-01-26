e8c54030966dd2281d88e8f6410ce9347b0f8026
Tasnim Mollah <tasnmmollah@gmail.com>
2023-01-26 15:44:54 -0500

    get all QAs api

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,11 @@
-const router = require('express').Router()
-module.exports = router
+const router = require("express").Router();
+module.exports = router;
 
-router.use('/users', require('./users'))
+router.use("/users", require("./users"));
+router.use("/questions", require("./question_answer"));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
