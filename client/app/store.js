0706333e2177975dfed29240169667f447a8dcfc
Tasnim Mollah <tasnmmollah@gmail.com>
2023-01-26 20:02:41 -0500

    started all questions component

--- client/app/store.js
+++ client/app/store.js
@@ -1,11 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
-import authReducer from '../features/auth/authSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import authReducer from "../features/auth/authSlice";
+import allQASliceReducer from "../features/allQA/allQASlice";
+import allQuestionsSliceReducer from "../features/allQA/allQuestionsSlice";
 
 const store = configureStore({
-  reducer: { auth: authReducer },
+  reducer: {
+    auth: authReducer,
+    questionsAnswers: allQASliceReducer,
+    questions: allQuestionsSliceReducer,
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
-export * from '../features/auth/authSlice';
+export * from "../features/auth/authSlice";
