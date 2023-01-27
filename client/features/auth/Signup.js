9e74fc813133b81a2c944b3a42351d46c9076ab1
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-01-27 15:50:16 -0500

    added in school field

--- client/features/auth/Signup.js
+++ client/features/auth/Signup.js
@@ -30,9 +30,10 @@ const SignUp = () => {
     evt.preventDefault();
     const firstName = evt.target.firstName.value;
     const lastName = evt.target.lastName.value;
+    const expertise = evt.target.expertiseLevel.value;
+    const school = evt.target.school.value;
     const email = evt.target.email.value;
     const password = evt.target.password.value;
-    const expertise = evt.target.expertiseLevel.value;
 
     dispatch(
       authenticate({
@@ -41,6 +42,7 @@ const SignUp = () => {
         email,
         password,
         expertise,
+        school,
         method: "signup",
       })
     );
@@ -95,6 +97,18 @@ const SignUp = () => {
 
         <Row className="p-2" style={{ margin: "0px", padding: "0px" }}>
           <Col>
+            <Form.Group controlId="expertiseLevel">
+              <Form.Label label="Expertise Level">Expertise Level</Form.Label>
+              <Form.Select aria-label="Default select example">
+                {expertiseLevel.map((level) => (
+                  <option key={level} value={level}>
+                    {level}
+                  </option>
+                ))}
+              </Form.Select>
+            </Form.Group>
+          </Col>
+          <Col>
             <Form.Group controlId="email">
               <Row style={{ margin: "0px" }}>
                 <Form.Label label="Email Address">Email Address</Form.Label>
@@ -112,63 +126,71 @@ const SignUp = () => {
               </Col>
             </Form.Group>
           </Col>
+        </Row>
 
+        <Row className="p-2" style={{ margin: "0px", padding: "0px" }}>
           <Col>
-            <Form.Group controlId="expertiseLevel">
-              <Form.Label label="Expertise Level">Expertise Level</Form.Label>
-              <Form.Select aria-label="Default select example">
-                {expertiseLevel.map((level) => (
-                  <option key={level} value={level}>
-                    {level}
-                  </option>
-                ))}
-              </Form.Select>
+            <Form.Group controlId="password">
+              <Row style={{ margin: "0px", padding: "0px" }}>
+                <Form.Label label="Password" style={{ paddingLeft: "16px" }}>
+                  Password
+                </Form.Label>
+              </Row>
+              <Row>
+                <Col sm={12}>
+                  <InputGroup>
+                    <Form.Control
+                      required
+                      type={passwordShown ? "text" : "password"}
+                      placeholder="Enter password"
+                    />
+                    <Button
+                      variant="outline-secondary"
+                      onClick={togglePassword}
+                      size="md"
+                      style={{ zIndex: 0 }}
+                    >
+                      {passwordShown ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
+                    </Button>
+                    <Form.Control.Feedback type="invalid">
+                      Please provide a password.
+                    </Form.Control.Feedback>
+                  </InputGroup>
+                </Col>
+              </Row>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="school">
+              <Row style={{ margin: "0px", padding: "0px" }}>
+                <Form.Label label="School Affiliation">School Affiliation</Form.Label>
+              </Row>
+              <Col sm={12}>
+                <InputGroup>
+                  <Form.Control
+                    style={{ borderRadius: "10px" }}
+                    type="text"
+                    placeholder="Enter school name"
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide your school affiliation.
+                  </Form.Control.Feedback>
+                </InputGroup>
+              </Col>
             </Form.Group>
           </Col>
         </Row>
 
-        <Row className="p-2" style={{ margin: "0px", padding: "0px" }}>
-          <Form.Group controlId="password">
-            <Row style={{ margin: "0px", padding: "0px" }}>
-              <Form.Label label="Password" style={{ paddingLeft: "16px" }}>
-                Password
-              </Form.Label>
-            </Row>
-            <Row>
-              <Col sm={6}>
-                <InputGroup>
-                  <Form.Control
-                    required
-                    type={passwordShown ? "text" : "password"}
-                    placeholder="Enter password"
-                  />
-                  <Button
-                    variant="outline-secondary"
-                    onClick={togglePassword}
-                    size="md"
-                    style={{ zIndex: 0 }}
-                  >
-                    {passwordShown ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
-                  </Button>
-                  <Form.Control.Feedback type="invalid">
-                    Please provide a password.
-                  </Form.Control.Feedback>
-                </InputGroup>
-              </Col>
-
-              <Col>
-                <Button
-                  style={{ textAlign: "right", justifyContent: "right", float: "right" }}
-                  id="buttons"
-                  variant="secondary"
-                  type="submit"
-                  size="md"
-                >
-                  Sign Up
-                </Button>
-              </Col>
-            </Row>
-          </Form.Group>
+        <Row className="p-2">
+          <Button
+            // style={{ textAlign: "right", justifyContent: "right", float: "right" }}
+            id="buttons"
+            variant="secondary"
+            type="submit"
+            size="md"
+          >
+            Sign Up
+          </Button>
         </Row>
       </Form>
       <div className="p-1"></div>
