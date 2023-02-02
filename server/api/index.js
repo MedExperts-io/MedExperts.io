4fcdb38c8891fc15a075b41039d8a9011036a764
Tasnim Mollah <tasnmmollah@gmail.com>
2023-02-02 01:57:34 -0500

    db for qa vcs

--- server/api/index.js
+++ server/api/index.js
@@ -4,7 +4,6 @@ module.exports = router;
 router.use("/users", require("./users"));
 router.use("/questions", require("./question_answer"));
 router.use("/user_questions", require("./user_question"));
-router.use("/qahistory", require("./qahistory"))
 
 router.use((req, res, next) => {
   const error = new Error("Not Found");
--- server/api/question_answer.js
+++ server/api/question_answer.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const {
-  models: { User, Question_Answer, User_Question, QAHistory },
+  models: { User, Question_Answer, User_Question },
 } = require("../db");
+const { Op } = require("sequelize");
 module.exports = router;
 
 const { getToken, isAdmin } = require("./userCheckMiddleware");
@@ -10,6 +11,7 @@ const { getToken, isAdmin } = require("./userCheckMiddleware");
 router.get("/", getToken, async (req, res, next) => {
   try {
     const allQs = await Question_Answer.findAll({
+      where: { status: "Active" },
       attributes: [
         "id",
         "question",
@@ -17,6 +19,7 @@ router.get("/", getToken, async (req, res, next) => {
         "answerOptions",
         "level",
         "category",
+        "status",
       ],
     });
     res.json(allQs);
@@ -25,34 +28,11 @@ router.get("/", getToken, async (req, res, next) => {
   }
 });
 
-//------- GET/api/questions/:singleQuestionId------- HAMZA'S CODE
-// router.get("/:singleQuestionId", async (req, res, next) => {
-//   try {
-//     const singleQuestion = await Question_Answer.findOne({
-//       where: { id: req.params.singleQuestionId },
-//     });
-//     if (singleQuestion) {
-//       res.json(singleQuestion);
-//     } else {
-//       res.json({ error: "Product not found" });
-//     }
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
-
-//-------------------------VERSION CONTROL FOR QUESTIONS-------------------------
-
-
-// OPTION 1 - MOVE TO NEW TABLE
-
-
-//--------GET---------/api/questions/:singleQuestionId
+//GET/api/questions/:singleQuestionId------- Hamza's coode FOR STUDENTS
 router.get("/:singleQuestionId", async (req, res, next) => {
   try {
     const singleQuestion = await Question_Answer.findOne({
-      where: { id: 1 }, include: {model: User}
+      where: { id: req.params.singleQuestionId },
     });
     if (singleQuestion) {
       res.json(singleQuestion);
@@ -64,63 +44,33 @@ router.get("/:singleQuestionId", async (req, res, next) => {
   }
 });
 
+//GET/api/questions/:singleQuestionId------- FOR ADMIN TO SEE VERSIONS, combine w above
+// router.get("/:singleQuestionId", getToken, isAdmin, async (req, res, next) => {
+//   const qaId = req.params.singleQuestionId;
+//   try {
+//     const singleQuestion = await Question_Answer.findOne({
+//       where: { id: qaId },
+//       include: {
+//         model: User_Question,
+//       },
+//     });
+//     if (singleQuestion) {
+//       const allVersions = await Question_Answer.findAll({
+//         order: [["createdAt", "DESC"]],
+//         where: {
+//           [Op.or]: [
+//             { id: singleQuestion.ancestorId },
+//             { ancestorId: singleQuestion.ancestorId },
+//           ],
+//         },
+//         include: User_Question,
+//       });
 
-
-//-----------POST----------------/api/questions/:singleQuestionId
-router.post("/:singleQuestionId", async (req, res, next) => {
-  const qaId = req.params.singleQuestionId;
-  try {
-// --------FIND QA instance
-    const singleQuestion = await Question_Answer.findOne({
-      where: { id: qaId },
-      include: { model: User },
-    });
-    if (singleQuestion) {
-// -------Create instance copy in new table with associations
-      const moveOld = await QAHistory.create({
-        questionAnswerId: qaId,
-        question: singleQuestion.question,
-        questionImage: singleQuestion.questionImage,
-        answerOptions: singleQuestion.answerOptions,
-        correctAnswer: singleQuestion.correctAnswer,
-        explanation: singleQuestion.explanation,
-        explanationImage: singleQuestion.explanationImage,
-        explanationLinks: singleQuestion.explanationLinks,
-        level: singleQuestion.level,
-        category: singleQuestion.category,
-        userQuestions:singleQuestion.users
-      })
-//----------Replace old instance with incoming post
-      const createNew = await Question_Answer.update(req.body)//WILL THIS REMOVE ASSOCIATIONS?
-
-    res.json(createNew);
-    } else {
-      res.json({ error: "Product not found" });
-    }
-  } catch (err) {
-    next(err);
-  }
-});
-
-
-
-// OPTION 2 - MOVE TO NEW FIELD
-router.post("/:singleQuestionId", async (req, res, next) => {
-  const qaId = req.params.singleQuestionId;
-  try {
-    // FIND QA instance
-    const singleQuestion = await Question_Answer.findOne({
-      where: { id: qaId },
-      include: { model: User },
-    });
-    if (singleQuestion) {
-      const createNew = await Question_Answer.update()//WILL THIS REMOVE ASSOCIATIONS?
-
-    res.json(createNew);
-    } else {
-      res.json({ error: "Product not found" });
-    }
-  } catch (err) {
-    next(err);
-  }
-});
+//       res.json(allVersions);
+//     } else {
+//       res.json({ error: "Question not found" });
+//     }
+//   } catch (err) {
+//     next(err);
+//   }
+// });
