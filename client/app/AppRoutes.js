6f51cd8f91ccaff3e6d811c950715e1becb691cc
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-02-08 15:03:40 -0500

    separated login/signup components & routes, added modal to signup component, created verifyemail component

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector, Navigate } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import QuestionsAnswers from "../features/allQA/AllQA";
 import LoginOrSignup from "../features/auth/LoginOrSignup";
+import Login from "../features/auth/Login";
+import SignUp from "../features/auth/Signup";
 // import Home from "../features/home/Home";
 import Profile from "../features/auth/Profile";
 import RequestNewPassword from "../features/auth/RequestNewPW";
@@ -13,6 +15,7 @@ import SingleQuestion from "../features/singleQuestion/SingleQuestion";
 import LoadingScreen from "../features/loading/LoadingScreen";
 import LandingPage from "../features/landingPage/LandingPage";
 import AllQAadmin from "../features/allQA/AllQAadmin";
+import VerifyEmail from "../features/auth/VerifyEmail";
 
 const AppRoutes = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
@@ -40,13 +43,17 @@ const AppRoutes = () => {
       ) : (
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginOrSignup />} />
+          <Route path="/home" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/forgotPassword" element={<RequestNewPassword />} />
           <Route
             path="/forgotPassword/confirmation"
             element={<PasswordRequestConfirmation />}
           />
           <Route path="/resetPassword/*" element={<ResetPassword />} />
+          {/* <Route path="/" element={<VerifyEmail />} /> */}
+          <Route path="/verifyEmail/*" element={<VerifyEmail />} />
           {/* <Route path="/*" element={<LoginOrSignup />} /> */}
         </Routes>
       )}
--- client/features/auth/Login.js
+++ client/features/auth/Login.js
@@ -1,7 +1,15 @@
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import React, { useState } from "react";
-import { Button, Form, InputGroup } from "react-bootstrap";
+import {
+  Button,
+  Form,
+  InputGroup,
+  Container,
+  Card,
+  Col,
+  Row,
+} from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { authenticate } from "../../app/store";
@@ -29,66 +37,88 @@ const Login = () => {
   };
 
   return (
-    <div className="mb-3 mt-md-4">
-      <div className="mb-3">
-        <Form onSubmit={handleSubmit} name="login">
-          <Form.Group className="mb-3" controlId="loginEmail">
-            <Form.Label className="text-center" label="Email Address">
-              Email address
-            </Form.Label>
-            <Form.Control
-              required
-              autoComplete="email"
-              type="email"
-              placeholder="Enter email"
-            />
-            <Form.Control.Feedback type="invalid">
-              Please provide a valid email address.
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="loginPassword">
-            <Form.Label label="Password">Password</Form.Label>
-            <InputGroup>
-              <Form.Control
-                required
-                autoComplete="current-password"
-                type={passwordShown ? "text" : "password"}
-                placeholder="Enter password"
-              />
-              <Button
-                variant="outline-secondary"
-                onClick={togglePassword}
-                size="md"
-                style={{ zIndex: 0 }}
-              >
-                {passwordShown ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
-              </Button>
+    <Container>
+      <Row className="vh-100 d-flex justify-content-center align-items-center">
+        <Col md={8} lg={6} s={10} xs={12}>
+          <Card className="shadow">
+            <Card.Header>Login</Card.Header>
+            <Card.Body>
+              <div className="mb-3 mt-md-4">
+                <div className="mb-3">
+                  <Form onSubmit={handleSubmit} name="login">
+                    <Form.Group className="mb-3" controlId="loginEmail">
+                      <Form.Label className="text-center" label="Email Address">
+                        Email address
+                      </Form.Label>
+                      <Form.Control
+                        required
+                        autoComplete="email"
+                        type="email"
+                        placeholder="Enter email"
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        Please provide a valid email address.
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="loginPassword">
+                      <Form.Label label="Password">Password</Form.Label>
+                      <InputGroup>
+                        <Form.Control
+                          required
+                          autoComplete="current-password"
+                          type={passwordShown ? "text" : "password"}
+                          placeholder="Enter password"
+                        />
+                        <Button
+                          variant="outline-secondary"
+                          onClick={togglePassword}
+                          size="md"
+                          style={{ zIndex: 0 }}
+                        >
+                          {passwordShown ? (
+                            <VisibilityOffIcon />
+                          ) : (
+                            <RemoveRedEyeIcon />
+                          )}
+                        </Button>
 
-              <Form.Control.Feedback type="invalid">
-                Please provide a password.
-              </Form.Control.Feedback>
-            </InputGroup>
-          </Form.Group>{" "}
-          <Form.Group className="mb-3" controlId="checkbox">
-            <p className="small">
-              <a
-                className="text"
-                style={{ color: "#FF6262" }}
-                href="/forgotPassword"
-              >
-                Forgot password?
-              </a>
-            </p>
-          </Form.Group>
-          <div className="d-grid">
-            <Button variant="secondary" type="submit">
-              Login
-            </Button>
-            {error ? <p>{error}</p> : null}
-          </div>
-        </Form>
-      </div>
-    </div>
+                        <Form.Control.Feedback type="invalid">
+                          Please provide a password.
+                        </Form.Control.Feedback>
+                      </InputGroup>
+                    </Form.Group>{" "}
+                    <Form.Group className="mb-3">
+                      <p className="small">
+                        <a
+                          className="text"
+                          style={{ color: "#FF6262" }}
+                          href="/forgotPassword"
+                        >
+                          Forgot password?
+                        </a>
+                      </p>
+                    </Form.Group>
+                    <div className="d-grid">
+                      <Button variant="secondary" type="submit">
+                        Login
+                      </Button>
+                      {error ? <p>{error}</p> : null}
+                    </div>
+                  </Form>
+                </div>
+              </div>
+            </Card.Body>
+            <Card.Footer>
+              <p className="small">
+                <a className="text" style={{ color: "gray" }} href="/signup">
+                  New to MedExperts? Create account.
+                </a>
+              </p>
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
