7cd1451fb278391284f616aaf869bca073f8ca93
Tasnim Mollah <tasnmmollah@gmail.com>
2023-02-11 22:03:56 -0500

    seed data edit

--- client/features/stats/user_questionsSlice.js
+++ client/features/stats/user_questionsSlice.js
@@ -3,100 +3,125 @@ import axios from "axios";
 const token = window.localStorage.getItem("token");
 
 // --------For admin's dashboard analytics (aggregate)--------------
-export const fetchAllUserQuestions = createAsyncThunk("fetchAllUserQuestions", async () => {
-  try {
-    const { data } = await axios.get(`/api/user_questions`, {
-      headers: {
-        authorization: token,
-      },
-    });
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-export const fetchByAnswerFrequency = createAsyncThunk("fetchByAnswerFrequency", async () => {
-  try {
-    const { data } = await axios.get(`/api/user_questions/frequency`, {
-      headers: {
-        authorization: token,
-      },
-    });
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// --------For admin's dashboard analytics (by expertise)--------------
-export const fetchExpertiseQuestions = createAsyncThunk("fetchExpertiseQuestions", async () => {
-  try {
-    const { data } = await axios.get(`/api/user_questions/expertise/all`, {
-      headers: {
-        authorization: token,
-      },
-    });
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// --------For logged in user's dashboard analytics--------------
-export const fetchUserQuestions = createAsyncThunk("fetchUserQuestions", async (userId) => {
-  try {
-    const { data } = await axios.get(`/api/user_questions/${userId}`, {
-      headers: {
-        authorization: token,
-      },
-    });
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// --------TO FAVORITE, UNFAVORITE, ANSWERED OR NOT--------------
-
-export const updateUserQuestion = createAsyncThunk("updateUserQuestion", async ({ userId, questionAnswerId }) => {
-  try {
-    const { data } = await axios.put(
-      `/api/user_questions/${userId}`,
-      {
-        questionAnswerId: questionAnswerId,
-      },
-      {
+export const fetchAllUserQuestions = createAsyncThunk(
+  "fetchAllUserQuestions",
+  async () => {
+    try {
+      const { data } = await axios.get(`/api/user_questions`, {
         headers: {
           authorization: token,
         },
-      }
-    );
-    console.log("THUNK", data);
-    return data;
-  } catch (error) {
-    console.log(error);
+      });
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   }
-});
+);
+
+export const fetchByAnswerFrequency = createAsyncThunk(
+  "fetchByAnswerFrequency",
+  async () => {
+    try {
+      const { data } = await axios.get(`/api/user_questions/frequency`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+// --------For admin's dashboard analytics (by expertise)--------------
+export const fetchExpertiseQuestions = createAsyncThunk(
+  "fetchExpertiseQuestions",
+  async () => {
+    try {
+      const { data } = await axios.get(`/api/user_questions/expertise/all`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+// --------For logged in user's dashboard analytics--------------
+export const fetchUserQuestions = createAsyncThunk(
+  "fetchUserQuestions",
+  async (userId) => {
+    try {
+      const { data } = await axios.get(`/api/user_questions/${userId}`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+// --------TO FAVORITE, UNFAVORITE--------------
+export const updateUserQuestion = createAsyncThunk(
+  "updateUserQuestion",
+  async ({ userId, questionAnswerId }) => {
+    try {
+      const { data } = await axios.put(
+        `/api/user_questions/${userId}`,
+        {
+          questionAnswerId: questionAnswerId,
+        },
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      );
+      console.log("THUNK", data);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 // --------TO STORE USER INPUT--------------
-export const updateUserQuestionInput = createAsyncThunk("updateUserQuestionInput", async ({ userId, questionAnswerId, userInput, answered, category, level, userExpertise }) => {
-  try {
-    const { data } = await axios.post(`/api/user_questions/${userId}`, {
-      questionAnswerId: questionAnswerId,
-      userInput: userInput,
-      answered: answered,
-      category: category,
-      level: level,
-      userExpertise: userExpertise,
-    });
-    // console.log("THUNK", data);
-    return data;
-  } catch (error) {
-    console.log(error);
+export const updateUserQuestionInput = createAsyncThunk(
+  "updateUserQuestionInput",
+  async ({
+    userId,
+    questionAnswerId,
+    userInput,
+    answered,
+    category,
+    level,
+    userExpertise,
+  }) => {
+    try {
+      const { data } = await axios.post(`/api/user_questions/${userId}`, {
+        questionAnswerId: questionAnswerId,
+        userInput: userInput,
+        answered: answered,
+        category: category,
+        level: level,
+        userExpertise: userExpertise,
+      });
+      // console.log("THUNK", data);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   }
-});
+);
 
 export const allUser_QuestionsSlice = createSlice({
   name: "allUser_Questions",
@@ -128,7 +153,9 @@ export const allUser_QuestionsSlice = createSlice({
             frequency: frequency[question.id],
           };
         });
-        const sortedByFrequency = allQuestions.sort((a, b) => b.frequency - a.frequency);
+        const sortedByFrequency = allQuestions.sort(
+          (a, b) => b.frequency - a.frequency
+        );
         const sortedByFrequencyReverse = sortedByFrequency.slice().reverse();
         // console.log("IN THE FREQUENCY BUILDER: SORTEDBYFREQUENCY: ", sortedByFrequency);
         // console.log("IN THE FREQUENCY BUILDER: SORTEDBYFREQUENCY REVERSE: ", sortedByFrequencyReverse);
@@ -140,9 +167,15 @@ export const allUser_QuestionsSlice = createSlice({
       })
       .addCase(fetchUserQuestions.fulfilled, (state, action) => {
         state.UserQuestions = action.payload;
-        state.userEasy = action.payload.filter((question) => question.level === "Easy" && question.userInput);
-        state.userModerate = action.payload.filter((question) => question.level === "Moderate" && question.userInput);
-        state.userHard = action.payload.filter((question) => question.level === "Hard" && question.userInput);
+        state.userEasy = action.payload.filter(
+          (question) => question.level === "Easy" && question.userInput
+        );
+        state.userModerate = action.payload.filter(
+          (question) => question.level === "Moderate" && question.userInput
+        );
+        state.userHard = action.payload.filter(
+          (question) => question.level === "Hard" && question.userInput
+        );
       })
       .addCase(updateUserQuestion.fulfilled, (state, action) => {
         // console.log("FAVORITED ACTION PAYLOAD", action.payload);
