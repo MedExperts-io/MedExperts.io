5d1ae14dd0fad17adc128473320fba876b51603e
Tasnim Mollah <tasnmmollah@gmail.com>
2023-02-11 22:03:56 -0500

    seed data edit

--- client/features/singleQuestion/singleQuestionSlice.js
+++ client/features/singleQuestion/singleQuestionSlice.js
@@ -22,7 +22,7 @@ export const fetchQAVersions = createAsyncThunk(
         authorization: token,
       },
     });
-    console.log("THUNK ALL VERSIONS", data);
+    //console.log("THUNK ALL VERSIONS", data);
     return data;
   }
 );
@@ -98,7 +98,7 @@ const singleQuestionSlice = createSlice({
         state.error = action.error;
       })
       .addCase(fetchQAVersions.fulfilled, (state, action) => {
-        console.log("BUILDER ALL VERSIONS", action.payload);
+        //console.log("BUILDER ALL VERSIONS", action.payload);
         state.qaAllVersions = action.payload;
       })
       .addCase(editQuestion.fulfilled, (state, action) => {
