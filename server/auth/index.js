793bc0d3633e5e47ab36f34273ba402c64f7dc2d
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-03-16 16:23:54 -0400

    modified env file

--- server/auth/index.js
+++ server/auth/index.js
@@ -15,11 +15,22 @@ const path = require("path");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
+// const transport = nodemailer.createTransport({
+//   host: process.env.HOST,
+//   service: process.env.SERVICE,
+//   port: process.env.PORT,
+//   secure: true,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
 const transport = nodemailer.createTransport({
   host: process.env.HOST,
-  service: process.env.SERVICE,
+  // service: process.env.SERVICE,
   port: process.env.PORT,
-  secure: true,
+  // secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
