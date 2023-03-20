0f856de5316890225f818463bf5fcdb9e95d2bba
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-03-20 12:38:23 -0400

    meet the team cards

--- client/features/aboutUs/AboutUs.js
+++ client/features/aboutUs/AboutUs.js
@@ -1,11 +1,40 @@
 import React from "react";
-import { Container, Card } from "react-bootstrap";
+import { Container, Card, Row, Col } from "react-bootstrap";
 
 const AboutUs = () => {
   return (
     <Container fluid>
-      <Card className="about-card mx-auto">
+      <Card className="about-card mx-auto" style={{ border: "none" }}>
         <h1 className="about-heading"> Meet the Team </h1>
+        <Row>
+          <Card id="member-card" as={Col}>
+            <Card.Img></Card.Img>
+            <Card.Body></Card.Body>
+            <Card.Title id="member-card-title">
+              <strong> Cody the Pug</strong>
+              <br />
+              Software Engineer
+            </Card.Title>
+          </Card>
+          <Card id="member-card" as={Col}>
+            <Card.Img></Card.Img>
+            <Card.Body></Card.Body>
+            <Card.Title id="member-card-title">
+              <strong>Brody the Bird</strong>
+              <br />
+              Software Engineer
+            </Card.Title>
+          </Card>
+          <Card id="member-card" as={Col}>
+            <Card.Img></Card.Img>
+            <Card.Body></Card.Body>
+            <Card.Title id="member-card-title">
+              <strong>Oscar the Grouch</strong>
+              <br />
+              Software Engineer
+            </Card.Title>
+          </Card>
+        </Row>
       </Card>
     </Container>
   );
