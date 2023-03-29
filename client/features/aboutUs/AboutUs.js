16da586b2f1e436e84824b4c395e18387d2d042b
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-03-29 15:00:44 -0400

    replaced pics longer v

--- client/features/aboutUs/AboutUs.js
+++ client/features/aboutUs/AboutUs.js
@@ -20,23 +20,25 @@ const AboutUs = () => {
         </Card.Header>
 
         <Row className="justify-content-center">
+          <p>Medical </p>
           {aboutData.map((member, idx) => {
             return (
-              <Card key={idx} id="member-card">
-                <Card.Body className="default">
-                  <Card.Img
-                    id="member-img"
-                    variant="top"
-                    src={member?.picture}
-                    alt={`${member?.firstName} ${member?.lastName}`}
-                  />
-                  <Card.Title>
-                    <p id="member-name1">
-                      {member?.firstName} {member?.lastName}
-                    </p>
-                    <p id="member-role1"> {member?.title}</p>{" "}
-                  </Card.Title>{" "}
-                  {/* <p id="member-desc1">
+              member.team === "Medical" && (
+                <Card key={idx} id="member-card">
+                  <Card.Body className="default">
+                    <Card.Img
+                      id="member-img"
+                      variant="top"
+                      src={member?.picture}
+                      alt={`${member?.firstName} ${member?.lastName}`}
+                    />
+                    <Card.Title>
+                      <p id="member-name1">
+                        {member?.firstName} {member?.lastName}
+                      </p>
+                      <p id="member-role1"> {member?.title}</p>{" "}
+                    </Card.Title>{" "}
+                    {/* <p id="member-desc1">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Aliquam fringilla purus ac ultricies facilisis. Integer
                     rutrum, ante et ultricies cursus, augue augue aliquet orci,
@@ -45,7 +47,7 @@ const AboutUs = () => {
                     habitasse platea dictumst. Integer sit amet urna id sapien
                     laoreet tincidunt in at felis.
                   </p> */}
-                  <p className="d-flex justify-content-start">
+                    {/* <p className="d-flex justify-content-start">
                     {member?.github && (
                       <a
                         target="_blank"
@@ -58,9 +60,56 @@ const AboutUs = () => {
                     <a target="_blank" style={{ color: "#FF6262" }}>
                       <LinkedInIcon />
                     </a>
-                  </p>
-                </Card.Body>{" "}
-              </Card>
+                  </p> */}
+                  </Card.Body>{" "}
+                </Card>
+              )
+            );
+          })}
+          <p>Engineering </p>
+          {aboutData.map((member, idx) => {
+            return (
+              member.team === "Engineering" && (
+                <Card key={idx} id="member-card">
+                  <Card.Body className="default">
+                    <Card.Img
+                      id="member-img"
+                      variant="top"
+                      src={member?.picture}
+                      alt={`${member?.firstName} ${member?.lastName}`}
+                    />
+                    <Card.Title>
+                      <p id="member-name1">
+                        {member?.firstName} {member?.lastName}
+                      </p>
+                      <p id="member-role1"> {member?.title}</p>{" "}
+                    </Card.Title>{" "}
+                    {/* <p id="member-desc1">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Aliquam fringilla purus ac ultricies facilisis. Integer
+                    rutrum, ante et ultricies cursus, augue augue aliquet orci,
+                    non feugiat diam metus sit amet eros. Nulla facilisi. Mauris
+                    sit amet egestas ligula. Suspendisse potenti. In hac
+                    habitasse platea dictumst. Integer sit amet urna id sapien
+                    laoreet tincidunt in at felis.
+                  </p> */}
+                    {/* <p className="d-flex justify-content-start">
+                    {member?.github && (
+                      <a
+                        target="_blank"
+                        href={member?.github}
+                        style={{ color: "#FF6262" }}
+                      >
+                        <GitHubIcon />
+                      </a>
+                    )}
+                    <a target="_blank" style={{ color: "#FF6262" }}>
+                      <LinkedInIcon />
+                    </a>
+                  </p> */}
+                  </Card.Body>{" "}
+                </Card>
+              )
             );
           })}
         </Row>
