ecf940db60cf94c7fd91185ec12b8f580caff24e
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-04-12 11:44:05 -0400

    routes and component for contact form

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -20,6 +20,7 @@ import VerifyEmail from "../features/auth/VerifyEmail";
 import AddQuestion from "../features/addQ/AddQuestion";
 import MainLandingPage from "../features/landingPage/MainLandingPage";
 import NoExist from "../features/doesNotExist/NoExist";
+import ContactForm from "../features/contact/ContactForm";
 
 const AppRoutes = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
@@ -52,6 +53,7 @@ const AppRoutes = () => {
             element={<EditQA />}
           />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/contact" element={<ContactForm />} />
         </Routes>
       ) : (
         <Routes>
@@ -66,6 +68,7 @@ const AppRoutes = () => {
           />
           <Route path="/resetPassword/*" element={<ResetPassword />} />
           <Route path="/verifyEmail/*" element={<VerifyEmail />} />
+          <Route path="/contact" element={<ContactForm />} />
         </Routes>
       )}
     </div>
---
+++ client/features/contact/ContactForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContactForm = () => {
+  return <div>Hold for contact page</div>;
+};
+
+export default ContactForm;
