8d347320ea71bfd90bacac4baebe017cca5c37c0
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-04-13 11:16:31 -0400

    contact info section;

--- client/features/contact/ContactForm.js
+++ client/features/contact/ContactForm.js
@@ -1,6 +1,13 @@
-import React, { useRef } from "react";
-import { Container, Card, Row, Col, Button, Form } from "react-bootstrap";
 import emailjs from "@emailjs/browser";
+import React, { useRef } from "react";
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  FloatingLabel,
+  Row,
+} from "react-bootstrap";
 
 const ContactForm = () => {
   const form = useRef();
@@ -28,31 +35,56 @@ const ContactForm = () => {
 
   return (
     <Container fluid>
-      {/* <Card className="mx-auto shadow"> */}
       <Col>
         <Form ref={form} onSubmit={sendEmail}>
-          <Form.Group className="mb-3" controlId="contact-name">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="text" />
-          </Form.Group>
+          <FloatingLabel className="mb-3" controlId="contact-name" label="Name">
+            <Form.Control type="text" placeholder="Your name" />
+          </FloatingLabel>
           {/* <label>Name</label>
             <input type="text" name="user_name" /> */}
-          <Form.Group className="mb-3" controlId="contact-email">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" />
-          </Form.Group>
+          {/* <Form.Group className="mb-3" controlId="contact-email" required> */}
+          <FloatingLabel
+            className="mb-3"
+            controlId="contact-email"
+            label="Email Address"
+          >
+            {/* <Form.Label>Email address</Form.Label> */}
+            <Form.Control type="email" placeholder="Your email address" />
+          </FloatingLabel>
+          {/* </Form.Group> */}
+          {/* <Form.Group className="mb-3" controlId="contact-phone" required> */}
+          <FloatingLabel
+            className="mb-3"
+            controlId="contact-phone"
+            label="Phone Number"
+          >
+            {/* <Form.Label>Phone Number</Form.Label> */}
+            <Form.Control type="tel" placeholder="Your phone number" />
+          </FloatingLabel>
+          {/* </Form.Group> */}
           {/* <label>Email</label>
             <input type="email" name="user_email" /> */}
           {/* <label>Message</label>
             <textarea name="message" /> */}
-          <Form.Group className="mb-3" controlId="contact-msg">
-            <Form.Label>Message</Form.Label>
-            <Form.Control type="text" />
-          </Form.Group>
-          <Button type="submit"> Submit </Button>
+          {/* <Form.Group className="mb-3" controlId="contact-msg" required> */}
+          {/* <Form.Label>Message</Form.Label> */}
+          <FloatingLabel
+            className="mb-3"
+            controlId="contact-msg"
+            label="Message"
+          >
+            <Form.Control
+              as="textarea"
+              style={{ height: "100px" }}
+              placeholder="Your message"
+            />
+          </FloatingLabel>
+          {/* </Form.Group> */}
+          <div className="d-grid gap-2">
+            <Button type="submit"> Submit </Button>
+          </div>
         </Form>
       </Col>
-      {/* </Card> */}
     </Container>
   );
 };
--- client/features/contact/ContactUs.js
+++ client/features/contact/ContactUs.js
@@ -1,32 +1,98 @@
 import React, { useRef } from "react";
 import { Container, Card, Row, Col, Button, Form } from "react-bootstrap";
 import ContactForm from "./ContactForm";
+import EmailIcon from "@mui/icons-material/Email";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 const ContactUs = () => {
   return (
-    <Container fluid style={{ marginTop: "35px" }}>
-      <h1 className="text-center">Contact Us</h1>
-      <p className="text-center" style={{ fontSize: "18px" }}>
-        Fill out the form and a member of our team will get back to you within
-        24 hours, or scroll down for more ways to get in touch.
-      </p>
+    <Container fluid style={{ marginTop: "35px", marginBottom: "35px" }}>
+      <Col lg={10} md={10} className="mx-auto">
+        <h1 className="text-center">Get in Touch</h1>
+        <p
+          className="text-center"
+          style={{ fontSize: "18px", fontWeight: "300" }}
+        >
+          We'd love to hear from you! If you have any questions or comments for
+          us, fill out the form below with all your details and we'll get back
+          to you within 48 hours.
+        </p>
+      </Col>
       <Card className="mx-auto shadow" style={{ width: "80%" }}>
         <Card.Body>
           <Row>
             <Col>
               <Card
                 style={{
-                  width: "90%",
+                  width: "100%",
                   backgroundColor: "#f5f3f2",
                   border: "none",
+                  height: "100%",
                 }}
               >
-                <Card.Title style={{ padding: "10px" }}>
-                  Contact MedExperts
+                <Card.Title
+                  style={{
+                    padding: "10px",
+                    fontWeight: "600",
+                    fontSize: "20px",
+                  }}
+                >
+                  Contact Information
                 </Card.Title>
+                <Card.Body
+                  className="me-auto"
+                  //   style={{ verticalAlign: "middle" }}
+                >
+                  <p
+                    style={{
+                      fontWeight: "300",
+                      fontSize: "14px",
+                    }}
+                  >
+                    <EmailIcon /> support@medexperts.io
+                  </p>
+                  <p style={{ fontWeight: "300", fontSize: "14px" }}>
+                    <LocationOnIcon />
+                    New York, NY
+                  </p>
+                  <hr style={{ width: "100%" }} />
+                  <p
+                    style={{
+                      fontWeight: "600",
+                      fontSize: "16px",
+                    }}
+                  >
+                    Connect with MedExperts on Social Media
+                  </p>
+                  <p style={{ fontWeight: "300", fontSize: "14px" }}>
+                    <LinkedInIcon /> LinkedIn
+                  </p>
+                  <p style={{ fontWeight: "300", fontSize: "14px" }}>
+                    <InstagramIcon /> Instagram
+                  </p>
+                  <p style={{ fontWeight: "300", fontSize: "14px" }}>
+                    <GitHubIcon /> GitHub
+                  </p>
+                </Card.Body>
               </Card>
             </Col>
-            <Col>{<ContactForm />}</Col>
+            <Col>
+              <Card style={{ border: "none" }}>
+                <Card.Title
+                  style={{
+                    padding: "10px",
+                    fontWeight: "600",
+                    fontSize: "20px",
+                  }}
+                >
+                  Send us a Message
+                </Card.Title>
+                {<ContactForm />}
+              </Card>
+            </Col>
           </Row>
         </Card.Body>
       </Card>
