540b93d0ca311cf0c3c72abd4cf97591ee8d46fd
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-04-19 11:15:17 -0400

    separated survey json

--- client/features/feedback/Feedback.js
+++ client/features/feedback/Feedback.js
@@ -1,10 +1,8 @@
 import React from "react";
-
-import "survey-core/defaultV2.min.css";
 import { Model } from "survey-core";
 import { Survey } from "survey-react-ui";
-import 
-
+import { surveyJson } from "./SurveyQuestions";
+import "survey-core/defaultV2.min.css";
 
 const Feedback = () => {
   const survey = new Model(surveyJson);
--- client/features/feedback/surveyQuestions.js
+++ client/features/feedback/surveyQuestions.js
@@ -63,3 +63,5 @@ const surveyJson = {
     },
   ],
 };
+
+module.exports = { surveyJson };
