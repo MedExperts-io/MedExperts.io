885490595d29e595c6d9fc8781e64b2991894785
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-04-20 17:52:10 -0400

    remove user response col

--- server/db/models/User_Feedback.js
+++ server/db/models/User_Feedback.js
@@ -2,9 +2,6 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const User_Feedback = db.define("user_feedback", {
-  userResponses: {
-    type: Sequelize.JSONB,
-  },
   source: {
     type: Sequelize.STRING,
   },
