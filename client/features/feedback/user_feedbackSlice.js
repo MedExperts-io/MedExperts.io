d6a71ac97957a6fe152d5d64fce0db9b51f50f87
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-04-21 12:40:16 -0400

    specific reducers added for feedback

--- client/features/feedback/user_feedbackSlice.js
+++ client/features/feedback/user_feedbackSlice.js
@@ -1,12 +1,80 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-// <-------- For admin's dashboard------->
+// <-------- For admin's dashboard: ALL survey data------->
 export const fetchAllUserFeedback = createAsyncThunk(
   "fetchAllUserFeedback",
   async () => {
     try {
       const { data } = await axios.get(`api/user_feedback`, {
+        headers: {
+          authorization: window.localStorage.getItem("token"),
+        },
+      });
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+// <-------- For admin's dashboard: SATISFACTION RATINGS survey data------->
+export const fetchSatisfactionFeedback = createAsyncThunk(
+  "fetchSatisfactionFeedback",
+  async () => {
+    try {
+      const { data } = await axios.get(`api/user_feedback/satisfaction`, {
+        headers: {
+          authorization: window.localStorage.getItem("token"),
+        },
+      });
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+// <-------- For admin's dashboard: SOURCE survey data------->
+export const fetchSourceFeedback = createAsyncThunk(
+  "fetchSourceFeedback",
+  async () => {
+    try {
+      const { data } = await axios.get(`api/user_feedback/source`, {
+        headers: {
+          authorization: window.localStorage.getItem("token"),
+        },
+      });
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+// <-------- For admin's dashboard: SUGGESTED FEATURES survey data------->
+export const fetchSuggestedFeaturesFeedback = createAsyncThunk(
+  "fetchSuggestedFeaturesFeedback",
+  async () => {
+    try {
+      const { data } = await axios.get(`api/user_feedback/suggestedFeatures`, {
+        headers: {
+          authorization: window.localStorage.getItem("token"),
+        },
+      });
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+// <-------- For admin's dashboard: OTHER Feedback survey data------->
+export const fetchOtherFeedback = createAsyncThunk(
+  "fetchOtherFeedback",
+  async () => {
+    try {
+      const { data } = await axios.get(`api/user_feedback/otherFeedback`, {
         headers: {
           authorization: window.localStorage.getItem("token"),
         },
@@ -56,7 +124,16 @@ export const user_FeedbackSlice = createSlice({
       })
       .addCase(fetchAllUserFeedback.fulfilled, (state, action) => {
         state.response = action.payload;
-      });
+      })
+      .addCase(fetchSatisfactionFeedback.fulfilled, (state, action) => {
+        state.response = action.payload;
+      })
+      // .addCase(fetchS.fulfilled, (state, action) => {
+      //   state.response = action.payload;
+      // })
+      // .addCase(fetchAllUserFeedback.fulfilled, (state, action) => {
+      //   state.response = action.payload;
+      // });
   },
 });
 
