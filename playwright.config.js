049885625163cc186016a09cfe3c556d44ebc31e
Tasnim Mollah <tasnmmollah@gmail.com>
2023-04-24 15:39:01 -0400

    admins allQA navigation tests added

--- playwright.config.js
+++ playwright.config.js
@@ -11,7 +11,7 @@ const { defineConfig, devices } = require("@playwright/test");
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  timeout: 5 * 60 * 1000,
+  timeout: 30000,
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -26,10 +26,11 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://localhost:8080/',
+
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "on",
   },
 
   /* Configure projects for major browsers */
--- tests/allQAadmin.spec.js
+++ tests/allQAadmin.spec.js
@@ -1,36 +1,61 @@
+// @ts-check
+
 import { test, expect } from '@playwright/test';
-require("dotenv").config();
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:8080/');
   await page.getByRole('button', { name: 'Login' }).click();
+  await page.locator('form[name="login"]').getByRole('button', { name: 'Login' }).click();
+  await page.getByText('Login').nth(1).click();
   await page.getByPlaceholder('Enter email').click();
-  await page.getByPlaceholder('Enter email').fill(`${process.env.ADMIN_U}`);
+  await page.getByPlaceholder('Enter email').fill('admin@medexperts.com');
+  await page.locator('form[name="login"]').getByRole('button', { name: 'Login' }).click();
+  await page.getByText('Login').nth(1).click();
   await page.getByPlaceholder('Enter password').click();
-  await page.getByPlaceholder('Enter password').fill(`${process.env.ADMIN_P}`);
+  await page.getByPlaceholder('Enter password').fill('pD*S3FJ%52QP7p');
   await page.locator('form[name="login"]').getByRole('button', { name: 'Login' }).click();
   await page.getByRole('button', { name: 'Questions' }).click();
+  await page.getByText('Student Progress').click();
+  await page.getByText('All Levels & All Categories').click();
+  await page.locator('.MuiChip-label').first().click();
+  // await page.getByRole('button', { name: 'Heart button for question 1. Not Favorited' }).click();
+  // await page.getByLabel('Favorites Only').check();
+  // await page.locator('.justify-content-center > .card > .card-header').click();
+  // await page.getByRole('button', { name: 'Heart button for question 1. Favorited' }).click();
+  // await page.getByLabel('Favorites Only').uncheck();
   await page.getByRole('link', { name: 'Question Number 1, difficulty: Moderate' }).click();
+  await page.getByText('Question 1: A 35-year-old female presents to the pulmonary clinic with frequent ').click();
+  await page.getByRole('cell', { name: 'Answer Options' }).click();
+  await page.getByRole('cell', { name: 'Responses' }).click();
   await page.getByRole('button', { name: 'View Explanation' }).click();
-
   await page.getByRole('button', { name: 'View References' }).click();
-
   await page.getByRole('link', { name: 'All Questions' }).click();
   await page.getByRole('button', { name: 'All Levels' }).click();
   await page.getByRole('button', { name: 'Easy' }).click();
-  await page.getByRole('link', { name: 'Question Number 15, difficulty: Easy' }).click();
-  await page.goto('http://localhost:8080/questions');
+  await page.getByText('Easy & All Categories').click();
   await page.getByRole('button', { name: 'All Categories' }).click();
-  await page.getByRole('button', { name: 'Asthma' }).nth(1).click();
+  await page.getByRole('button', { name: 'Bronchiectasis' }).nth(1).click();
+  await page.getByText('Easy & Bronchiectasis').click();
   await page.getByRole('button', { name: 'All Expertise' }).click();
-  await page.getByRole('button', { name: 'Student' }).click();
-  await page.getByRole('button', { name: 'Student' }).click();
-  await page.getByRole('button', { name: 'All Expertise' }).click();
+  await page.getByRole('button', { name: 'All Expertise' }).first().click();
   await page.getByRole('button', { name: 'Frequency Sort' }).click();
   await page.getByRole('button', { name: 'Percent Correct Sort' }).click();
-  await page.getByRole('button', { name: 'All Levels' }).click();
-  await page.getByRole('button', { name: 'Moderate' }).click();
-
-  await page.getByRole('link', { name: 'Question Number 87, difficulty: Moderate' }).click();
+  await page.getByRole('link', { name: 'Question Number 39, difficulty: Easy' }).click();
+  await page.getByText('Question 39: Which of the following is the most common secondary complication of').click();
+  await page.getByRole('button', { name: 'Delete Version' }).click();
+  await page.locator('div').filter({ hasText: 'Delete version with unique ID 39?' }).nth(3).click();
+  await page.getByRole('button', { name: 'Cancel' }).click();
   await page.getByRole('link', { name: 'All Questions' }).click();
+  await page.getByRole('button', { name: 'Pulmonary Function Testing' }).click();
+  await page.getByText('All Levels & Pulmonary Function Testing').click();
+  await page.locator('#dropdown-basic').nth(1).click();
+  await page.getByRole('button', { name: 'All Categories' }).nth(1).click();
+  await page.getByRole('button', { name: '...' }).click();
+  await page.getByRole('button', { name: 'Page 5' }).click();
+  await page.getByRole('button', { name: 'Profile' }).click();
+  await page.getByText('Edit My Profile').click();
+  await page.getByRole('button', { name: 'Logout' }).click();
+  await page.getByRole('heading', { name: 'Welcome to MedExperts' }).click();
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByText('Login').nth(1).click();
 });
