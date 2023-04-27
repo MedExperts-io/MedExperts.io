6630279561bee621486d8641e830b572b2553959
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-04-26 20:34:15 -0400

    useEffect

--- client/features/contact/ContactForm.js
+++ client/features/contact/ContactForm.js
@@ -1,18 +1,15 @@
 import emailjs from "@emailjs/browser";
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Button, Col, Container, Form, FloatingLabel } from "react-bootstrap";
 import MessageSuccess from "./MessageSuccess";
 import ReCAPTCHA from "react-google-recaptcha";
-import axios from "axios";
 import { contactFormResponse } from "./contactFormSlice";
 
 const ContactForm = () => {
   const form = useRef();
   const dispatch = useDispatch();
-  const { response } = useSelector((state) => state.contactForm);
 
-  console.log(response, "response");
   const [formData, setFormData] = useState({
     from_name: "",
     from_email: "",
@@ -30,57 +27,64 @@ const ContactForm = () => {
     setFormData({ ...formData, [event.target.name]: event.target.value });
   };
 
-  const sendEmail = async (captchaVal, event) => {
-    console.log("captchaVal from inside", captchaVal);
-    event.preventDefault();
-    const params = {
-      "g-recaptcha-response": captchaVal,
-      ...formData,
-    };
+  const { response } = useSelector((state) => state.contactForm);
 
-    await dispatch(contactFormResponse({ token: captchaVal }));
-
-    if (response.status === "OK" || response == "OK") {
-      console.log("i'm ok");
-      emailjs.send(
-        process.env.REACT_APP_SERVICE_ID,
-        process.env.REACT_APP_TEMPLATE_ID,
-        params,
-        process.env.REACT_APP_PUBLIC_KEY
-      );
-      setMessageSent(true);
-      form.current.reset();
-    } else {
-      console.log("error in dispatch");
-    }
-
-    // try {
-    //   const response = await axios.post("/recaptcha", { token: captchaVal });
-    //   console.log(response.status, "RESPONSE>STATUS SENDEMAIL,");
-    //   if (response.status === 200) {
-    //     await emailjs.send(
-    //       process.env.REACT_APP_SERVICE_ID,
-    //       process.env.REACT_APP_TEMPLATE_ID,
-    //       params,
-    //       process.env.REACT_APP_PUBLIC_KEY
-    //     );
-    //     setMessageSent(true);
-    //     form.current.reset();
-    //   } else {
-    //     // setCaptchaError("Error verifying ReCAPTCHA token");
-    //     console.log("Error verifying ReCAPTCHA token");
-    //   }
-    // } catch (error) {
-    //   console.log(error);
-    // }
+  const params = {
+    "g-recaptcha-response": captchaVal,
+    ...formData,
   };
+  const sendEmail = (event) => {
+    event.preventDefault();
+
+    dispatch(contactFormResponse({ token: captchaVal }));
+  };
+
+  useEffect(() => {
+    if (response === "OK" && captchaVal !== null && !messageSent) {
+      emailjs
+        .send(
+          process.env.REACT_APP_SERVICE_ID,
+          process.env.REACT_APP_TEMPLATE_ID,
+          params,
+          process.env.REACT_APP_PUBLIC_KEY
+        )
+        .then(() => {
+          setMessageSent(true);
+          form.current.reset();
+          dispatch(contactFormResponse(""));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [response, captchaVal, messageSent]);
+
+  // try {
+  //   const response = await axios.post("/recaptcha", { token: captchaVal });
+  //   console.log(response.status, "RESPONSE>STATUS SENDEMAIL,");
+  //   if (response.status === 200) {
+  //     await emailjs.send(
+  //       process.env.REACT_APP_SERVICE_ID,
+  //       process.env.REACT_APP_TEMPLATE_ID,
+  //       params,
+  //       process.env.REACT_APP_PUBLIC_KEY
+  //     );
+  //     setMessageSent(true);
+  //     form.current.reset();
+  //   } else {
+  //     // setCaptchaError("Error verifying ReCAPTCHA token");
+  //     console.log("Error verifying ReCAPTCHA token");
+  //   }
+  // } catch (error) {
+  //   console.log(error);
+  // }
 
   return (
     <>
       {!messageSent ? (
         <Container fluid>
           <Col>
-            <Form ref={form} onSubmit={(event) => sendEmail(captchaVal, event)}>
+            <Form ref={form} onSubmit={(event) => sendEmail(event)}>
               <FloatingLabel
                 className="mb-3"
                 controlId="contact-name"
@@ -140,6 +144,7 @@ const ContactForm = () => {
                   onChange={(captchaValue) => {
                     setCaptchaVal(captchaValue);
                   }}
+                  timeout={5000}
                 />
 
                 <br />
