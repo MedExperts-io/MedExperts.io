63101e25ac50646a392cc5850391ae3c4b592a4b
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-05-09 12:16:21 -0400

    cleaned up about us file

--- client/features/aboutUs/AboutUs.js
+++ client/features/aboutUs/AboutUs.js
@@ -1,15 +1,10 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { Container, Card, Row, Button, Col } from "react-bootstrap";
-const { aboutData } = require("./aboutData");
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import React from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+const { aboutData } = require("./aboutData");
 
 const AboutUs = () => {
-  const medicalTeam = aboutData
-    .filter((member) => member.team === "Medical")
-    .sort((a, b) => a.lastName.localeCompare(b.lastName));
-
   const engineeringTeam = aboutData
     .filter((member) => member.team === "Engineering")
     .sort((a, b) => a.lastName.localeCompare(b.lastName));
@@ -21,9 +16,7 @@ const AboutUs = () => {
           className="about-heading"
           style={{
             color: "black",
-            // backgroundColor: "#f8d4d4",
             border: "none",
-            // textDecoration: "underline",
           }}
         >
           Our Mission
@@ -59,70 +52,12 @@ const AboutUs = () => {
           className="about-heading"
           style={{
             color: "black",
-            // backgroundColor: "#ff9090",
             border: "none",
           }}
         >
           Meet the Team
         </Card.Header>
-        {/* <------------------------------------Medical Team -----------------------------------> */}
-        {/* <h1 className="dept-heading me-auto" style={{ width: "15%" }}>
-          Medical
-        </h1>
-        <Row className="justify-content-center">
-          {medicalTeam.map((member, idx) => {
-            return (
-              <Card key={idx} id="member-card">
-                <Card.Body>
-                  <div className="member-card slide-up">
-                    <Card.Img
-                      className="member-img"
-                      variant="top"
-                      src={member?.picture}
-                      alt={`${member?.firstName} ${member?.lastName}`}
-                    />{" "}
-                    <div className="member-caption">
-                      {" "}
-                      <Card.Title>
-                        <p id="member-name1">
-                          {member?.firstName} {member?.lastName}
-                        </p>
-                        <p id="member-role1"> {member?.title}</p>{" "}
-                      </Card.Title>{" "}
-                      <p id="member-desc1">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Aliquam fringilla purus ac ultricies facilisis. Integer
-                        rutrum, ante et ultricies cursus, augue augue aliquet
-                        orci, non feugiat diam metus sit amet eros.
-                      </p>
-                      <p
-                        className="d-flex justify-content-start"
-                        style={{ paddingTop: "0", marginTop: "0" }}
-                      >
-                        {member?.github && (
-                          <a
-                            target="_blank"
-                            href={member?.github}
-                            style={{ color: "black" }}
-                          >
-                            <GitHubIcon />
-                          </a>
-                        )}
-                        <a target="_blank" style={{ color: "black" }}>
-                          <LinkedInIcon />
-                        </a>
-                      </p>
-                    </div>
-                  </div>
-                </Card.Body>{" "}
-              </Card>
-            );
-          })}
-        </Row>{" "} */}
-        {/* <------------------------------------Engineering Team -----------------------------------> */}
-        {/* <h1 className="dept-heading me-auto" style={{ width: "15%" }}>
-          Engineering
-        </h1> */}
+
         <Row className="justify-content-center">
           {engineeringTeam.map((member, idx) => {
             return (
