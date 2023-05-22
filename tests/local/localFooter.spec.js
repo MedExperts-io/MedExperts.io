593c54df0ca1c962a5bd131c85d06f4f38292170
Cynthia Brito-Mena <cynthia.britom@gmail.com>
2023-05-22 17:36:30 -0400

    locator modified

--- tests/local/localFooter.spec.js
+++ tests/local/localFooter.spec.js
@@ -7,18 +7,31 @@ test("test", async ({ page, context }) => {
 
   const [link1Target] = await Promise.all([
     context.waitForEvent("page"),
-    page.click('a[data-testid="github-link"]'),
+    page.locator("#footer").getByTestId("github-link").click(),
   ]);
+  await link1Target.waitForLoadState();
+  await expect(link1Target).toHaveURL(
+    "https://github.com/MedExperts-io/MedExperts.io"
+  );
 
   const [link2Target] = await Promise.all([
     context.waitForEvent("page"),
-    page.click('a[data-testid="insta-link"]'),
+    page.locator("#footer").getByTestId("insta-link").click(),
   ]);
+  await link2Target.waitForLoadState();
+  await expect(link2Target).toHaveURL(
+    "https://www.instagram.com/medexperts.io/"
+  );
 
   const [link3Target] = await Promise.all([
     context.waitForEvent("page"),
-    page.click('a[data-testid="linkedin-link"]'),
+    page.locator("#footer").getByTestId("linkedin-link").click(),
   ]);
+  await link3Target.waitForLoadState();
+  const url = await link3Target.url();
+  const expectedURLRegex =
+    /https:\/\/www\.linkedin\.com\/company\/medexperts-io(\/\?original_referer=.*)?/;
+  expect(url).toMatch(expectedURLRegex);
 
   await page.getByRole("link", { name: "Contact Us" }).click();
   await page.getByRole("link", { name: "About Us" }).click();
